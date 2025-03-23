4d78672dbcf3468c5ef2626a1cd4e3836182c3f1
mladenovicdev <acamladenovic23@gmail.com>
2025-03-23 15:16:21 +0100

    Initial commit - Added Assets folder

---
+++ Assets/Scripts/Chess Game/BoardLayout.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TeamColor
+{
+    Black, White
+}
+
+public enum PieceType
+{
+    Pawn, Bishop, Knight, Rook, Queen, King
+}
+
+public class BoardLayout : ScriptableObject 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
