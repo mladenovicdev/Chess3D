763c5a1fabc5a633ced79f5337cb5fc15c359de4
mladenovicdev <acamladenovic23@gmail.com>
2025-03-24 19:46:33 +0100

    Worked on some game scripts

--- Assets/Scripts/Chess Game/BoardLayout.cs
+++ Assets/Scripts/Chess Game/BoardLayout.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public enum TeamColor
 {
-    Black, White
+    Black , White
 }
 
 public enum PieceType
@@ -12,17 +13,40 @@ public enum PieceType
     Pawn, Bishop, Knight, Rook, Queen, King
 }
 
+[CreateAssetMenu(menuName = "ScriptableObjects/Board/Layout")]
 public class BoardLayout : ScriptableObject 
 {
-    // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    private class BoardSquareSetup
     {
-        
+        public Vector2Int position;
+        public PieceType pieceType;
+        public TeamColor teamColor;
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private BoardSquareSetup[] boardSquares;
+
+    public int GetPiecesCount()
     {
-        
+        return boardSquares.Length;
+    }
+
+    //Coords of figure via index
+    public Vector2Int GetSquareCoordsAtIndex(int index)
+    {
+        return new Vector2Int(boardSquares[index].position.x - 1, boardSquares[index].position.y - 1);
+    }
+
+    //returns name of the figure as string via index
+    public string GetSquarePieceNameAtIndex(int index)
+    {
+        return boardSquares[index].pieceType.ToString();
+    }
+
+    //returns figure color via index
+    public TeamColor GetSquareColorAtIndex(int index)
+    {
+        return boardSquares[index].teamColor;
     }
 }
